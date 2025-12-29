366be576c5e1616ce970e5fad3812b5606101247
Björn Kübli <bjoern.kuebli@protonmail.com>
2025-12-29 13:04:59 +0100

    test: add unittests for class db

---
+++ app/db/test_db.py
@@ -0,0 +1,26 @@
+import unittest
+
+from app.db.db import DB
+
+
+class TestDB(unittest.TestCase):
+    def setUp(self) -> None:
+        self.db = DB(":memory:")
+
+    def test_tables_exist(self):
+        conn = self.db.connect()
+        cur = conn.cursor()
+
+        # check if table categories exists
+        cur.execute(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name='categories'"
+        )
+        self.assertIsNotNone(cur.fetchone())
+
+        # check if table categories exists
+        cur.execute(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name='articles'"
+        )
+        self.assertIsNotNone(cur.fetchone())
+
+        conn.close()
