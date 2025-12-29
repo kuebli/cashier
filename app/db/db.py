2f7c5ee758e6f268854f213c559e3c16c99d6d77
Björn Kübli <bjoern.kuebli@protonmail.com>
2025-12-29 13:00:45 +0100

    feat: make db connection persistent

--- app/db/db.py
+++ app/db/db.py
@@ -4,29 +4,37 @@ import sqlite3
 class DB:
     def __init__(self, db_name: str = "cashier.db") -> None:
         self.__db_name = db_name
+
+        self.conn = sqlite3.connect(self.__db_name)
+
+        # enforce foreign keys for this connection
+        self.conn.execute("PRAGMA foreign_keys = ON")
+
         self.__init_schema()
 
-    def __connect(self) -> sqlite3.Connection:
-        return sqlite3.connect(self.__db_name)
-
     def __init_schema(self) -> None:
-        with self.__connect() as conn:
-            cur = conn.cursor()
+        cur = self.conn.cursor()
 
-            cur.execute("""
-            CREATE TABLE IF NOT EXISTS categories (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL
-            )
-            """)
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS categories (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        )
+        """)
 
-            cur.execute("""
-            CREATE TABLE IF NOT EXISTS articles (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
-                price FLOAT NOT NULL,
-                category_id INTEGER NOT NULL,
-                FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
-            )
-            """)
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS articles (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            price FLOAT NOT NULL,
+            category_id INTEGER NOT NULL,
+            FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+        )
+        """)
+
+    def connect(self) -> sqlite3.Connection:
+        return self.conn
+
+    def close(self) -> None:
+        self.conn.close()
 
